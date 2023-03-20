0bc23f535c9208a740363768e0d4b1793ac9992b
real-tea <akash.0711a@gmail.com>
2023-03-20 23:57:45 +0530

    scrollview error

--- src/screens/Habits.tsx
+++ src/screens/Habits.tsx
@@ -90,6 +90,7 @@ export function Habit(){
     return(
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView
+            
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom : 100 }}>
                <BackButton/>
