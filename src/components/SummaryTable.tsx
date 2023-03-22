11f6c4bd955d59287562ce9d8277baa9fbbf108a
real-tea <akash.0711a@gmail.com>
2023-03-22 23:20:42 +0530

    Home Page/ClassName ⚠

---
+++ src/components/SummaryTable.tsx
@@ -0,0 +1,13 @@
+import { useNavigation , useFocusEffect  } from "@react-navigation/native"
+import dayjs from "dayjs"
+import { useCallback , useState } from 'react';
+import { View , Text , ScrollView , Alert } from "react-native";
+import { api } from "../lib/axios";
+// improt { api }
+import { Loading } from "./Loading";
+
+export const SummaryTable = () => {
+  return (
+    <div>SummaryTable</div>
+  )
+}
