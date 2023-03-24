19f008cef2d61cb1f2d9ee0875d15564211f455c
real-tea <akash.0711a@gmail.com>
2023-03-24 08:55:44 +0530

    screen routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,14 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
-
+import { Habit } from "../screens/Habits";
+import { Home } from "../screens/Home";
 
 const { Navigator , Screen } = createNativeStackNavigator();
 
 export function AppRoute(){
     return(
-        <Navigator>
-            <Screen name = "Home"/>
-            <Screen name = "Habit"/>
-            <Screen name = "NewHabit"/>
+        <Navigator screenOptions={{ headerShown : false }}>
+            <Screen name="Home" component={Home}/>
+            <Screen name="Habit" component={Habit}/>
+            {/* <Screen name="NewHabit" component={NewHabit}/> */}
         </Navigator>
     )
 }
